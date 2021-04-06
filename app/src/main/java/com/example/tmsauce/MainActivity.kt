07d52ebcd41983398368feccf67bf70a310b1e23
albertcodes <albertnthiwa@gmail.com>
2021-04-06 20:10:07 +0300

    progress

--- app/src/main/java/com/example/tmsauce/MainActivity.kt
+++ app/src/main/java/com/example/tmsauce/MainActivity.kt
@@ -3,9 +3,17 @@ package com.example.tmsauce
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+
+public class MainActivity : AppCompatActivity() {
+
+//    private Button registerButton, loginButton;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        registerButton = (Button) findViewById(R.id.register_button);
+//        loginButton = (Button) findViewById (R.id.login_button);
     }
 }
+
