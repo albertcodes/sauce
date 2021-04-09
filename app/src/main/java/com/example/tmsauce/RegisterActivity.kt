3f81ef37fd8c4d63410e17779c4f3aafe238a43a
albertcodes <albertnthiwa@gmail.com>
2021-04-09 19:48:28 +0300

    © TMSauce

--- app/src/main/java/com/example/tmsauce/RegisterActivity.kt
+++ app/src/main/java/com/example/tmsauce/RegisterActivity.kt
@@ -4,27 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 
-
-public class MainActivity : AppCompatActivity() {
-
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
 
         // get reference to button
-        val register_button = findViewById(R.id.register_button) as Button
+        val back_to_login_button = findViewById(R.id.back_to_login_button) as Button
         // set on-click listener
-        register_button.setOnClickListener {
+        back_to_login_button.setOnClickListener {
             // your code to perform when the user clicks on the button
             // Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, RegisterActivity::class.java).apply{
+            val intent = Intent(this, MainActivity::class.java).apply{
             }
             startActivity(intent)
         }
 
     }
-
 }
-
